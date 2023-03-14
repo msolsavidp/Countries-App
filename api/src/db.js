5cc8401d10d1b85040579df72eb706402802d447
msolsavidp <69515039+msolsavidp@users.noreply.github.com>
2023-03-14 19:57:48 -0300

    comas deleted

--- api/src/db.js
+++ api/src/db.js
@@ -10,7 +10,7 @@ const modelActivity = require ('./models/Activity');
 
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  native: false // lets Sequelize know we can use pg-native for ~30% more speed
 });
 const basename = path.basename(__filename);
 
