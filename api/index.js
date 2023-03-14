b5a2a3be0c26a512d7243bccd567f78191a37972
msolsavidp <69515039+msolsavidp@users.noreply.github.com>
2023-03-14 16:58:20 -0300

    dotenv

--- api/index.js
+++ api/index.js
@@ -22,10 +22,11 @@ const { conn } = require('./src/db.js');
 const axios = require ('axios');
 const {Country} = require ('./src/db');
 const { all } = require('./src/app.js');
+require('dotenv').config();
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, async () => {
+  server.listen(process.env.PORT, async () => {
     //Consulto si la base de datos tiene algo
     const allCountries = await Country.findAll();
 
@@ -34,7 +35,7 @@ conn.sync({ force: false }).then(() => {
     } else {
         console.log('The database is charged.')
     }
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at ', process.env.PORT); // eslint-disable-line no-console
   });
 });
 
@@ -63,36 +64,3 @@ const getInfo = () => axios.get('https://restcountries.com/v3/all',{
 });
 
 
-
-
-
-// conn.sync({ force: true }).then(() => {
-//     server.listen(3001, () => {
-//       getInfo();
-//       console.log('%s listening at 3001'); // eslint-disable-line no-console
-//     });
-//   });
-  
-//   //Checkear - VER findorcreate
-//   const getInfo = () => axios.get('https://restcountries.com/v3/all',{
-//       headers: {
-//           "accept-encoding": null
-//       }
-//   })
-//   .then(async r => {
-//       // await Country.sync({ force: true })
-//       r.data.map(async c => {
-//           await Country.create({
-//               id:c.cca3,
-//               ///////VER las traducciones de los nombres de los países y también los símbolos que tienen y traen consigo de la API /////////
-//               name: c.name.common && c.translations.spa.common,
-//               image: c.flags[0],
-//               continent: c.region,
-//               capital: typeof c.capital !== 'undefined' ? c.capital[0] : 'No se encontró la capital en la base de datos',
-//               subregion: typeof c.subregion !== 'undefined' ? c.subregion : 'No hay subregión para este país',
-//               area: c.area,
-//               population: c.population
-//           })
-//           // console.log (r.data)
-//       });
-//   });
