db720585247e42f68cc93259b33e68f04ecccaaf
msolsavidp <69515039+msolsavidp@users.noreply.github.com>
2023-03-14 20:52:45 -0300

    dbase

--- api/src/db.js
+++ api/src/db.js
@@ -8,7 +8,7 @@ const {
 const modelCountry = require ('./models/Country');
 const modelActivity = require ('./models/Activity');
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false // lets Sequelize know we can use pg-native for ~30% more speed
 });
